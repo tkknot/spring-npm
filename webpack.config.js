c45bb853c570669337c921e8a4256bdea603a4e9
tkknot <takeda19991015@gmail.com>
2022-08-17 20:22:41 +0900

    サンプルが完成しました

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,41 @@
-// webpack.config.js 抜粋
 const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const RemoveEmptySctiptsPlugin = require("webpack-remove-empty-scripts");
 
 module.exports = {
   entry: {
-    main: path.join(__dirname, "/src/main/resources/static/js/main.js"),
-    style: path.join(__dirname, "/src/main/resources/static/css/style.css"),
-    vendor: [
-      "axios",
-      "jquery",
-      "tailwindcss",
-    ],
+    // バンドル対象ファイルが一つの場合
+    // 生成されるファイル名:対象ファイルの位置
+    main: path.join(__dirname, '/src/main/resources/static/js/main.js'),
+    style: path.join(__dirname, '/src/main/resources/static/css/style.scss'),
+    // バンドル対象のファイルが複数の場合
+    vendor: ['axios', 'jquery'],
   },
   target: 'node',
+  // バンドルファイルの出力先
   output: {
-    path: path.join(__dirname, "/src/main/resources/static"), // eslint-disable-line
-    publicPath: "/",
-    filename: "js/[name].bundle.js",
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vender: {
-          chunks: "initial",
-        },
-      },
-    },
+    path: path.join(__dirname, '/src/main/resources/static'), // eslint-disable-line
+    publicPath: '/',
+    filename: 'js/[name].bundle.js',
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
+      $: 'jquery',
+      jQuery: 'jquery',
     }),
     new MiniCssExtractPlugin({
-      filename: "css/[name].css",
+      // cssを別ファイルとして書き出し
+      filename: './css/[name].css',
     }),
+
+    // バンドルの際に不要なファイルを削除
+    new RemoveEmptySctiptsPlugin(),
   ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
   module: {
     rules: [
       {
@@ -44,9 +43,9 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
             },
           },
         ],
@@ -54,22 +53,17 @@ module.exports = {
       {
         test: /\.(sc|c)ss$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "/css",
-            },
-          },
+          MiniCssExtractPlugin.loader,
           // .css 内の URL パスなどをそれぞれの publicPath に合わせてくれる
-          "css-loader",
+          'css-loader',
           // .sass のビルド
-          "sass-loader",
+          'sass-loader',
         ],
       },
     ],
   },
   resolve: {
-    extensions: [".js"],
-    modules: ["node_modules"],
+    extensions: ['.js'],
+    modules: ['node_modules'],
   },
 };
